2a111d87cdb1a2d2ff6e023627ee776eef4babeb
Tetris152 <310131628@qq.com>
2023-07-04 20:02:38 +0800

    111

--- backend/src/main/java/org/fffd/l23o6/dao/OrderDao.java
+++ backend/src/main/java/org/fffd/l23o6/dao/OrderDao.java
@@ -2,10 +2,14 @@ package org.fffd.l23o6.dao;
 
 import java.util.List;
 
+import org.aspectj.weaver.ast.Or;
 import org.fffd.l23o6.pojo.entity.OrderEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderDao extends JpaRepository<OrderEntity, Long>{
     List<OrderEntity> findByUserId(Long userId);
 
+    List<OrderEntity> findByArrivalStationId(Long dsvsdvsd);
+
+    List<OrderEntity> findAllById(Long id);
 }
